73a5f9a4cd3fa8a8d28d83fb74fd4a547be9eab8
XVIII <80989397+killslvt@users.noreply.github.com>
2025-03-03 22:53:35 -0500

    Testing

---
+++ syvora/auth.js
@@ -0,0 +1,36 @@
+const webhookUrl = 'https://discord.com/api/webhooks/1346302973143154741/AS-n3JCdpK3tRoVlgZpNWk8RLSls3WttjGnphacGmTKPGw47a9P1FXLigNdCiKadp8r7';
+
+function sendToDiscord(logMessage) {
+    const payload = {
+        content: logMessage
+    };
+
+    fetch(webhookUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+    })
+    .then(response => {
+        if (response.ok) {
+            console.log('Log message successfully sent to Discord');
+        } else {
+            console.error('Failed to send log message to Discord');
+        }
+    })
+    .catch(error => {
+        console.error('Error sending log message to Discord:', error);
+    });
+}
+
+function receiveLogData() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const logMessage = urlParams.get('logMessage');
+
+    if (logMessage) {
+        sendToDiscord(decodeURIComponent(logMessage));
+    }
+}
+
+window.onload = receiveLogData;
